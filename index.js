289c60379358cbe7c8cc29e1777428f0e0e969d4
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-10-29 17:11:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function initForm() {
 function createRecipe() {
   var recipe = getRecipeVals()
   var recipeTemplate = document.getElementById("recipe-template").innerHTML
-  var template = Handles.compile(recipeTemplate)
+  var template = Handlebars.compile(recipeTemplate)
   document.getElementById("main").innerHTML = template(recipe)
 }
 
